e5060ccaefd20e74994792ab6cad0d0c6d03b28e
Alessandro Esposito <alessandro.esposito251@gmail.com>
2026-07-20 16:51:35 +0200

    app setup

--- src/app/features/new-transaction/new-transaction.ts
+++ src/app/features/new-transaction/new-transaction.ts
@@ -1,9 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { TuiButton } from "@taiga-ui/core";
+import { TuiSheetDialog, TuiSheetDialogOptions } from "@taiga-ui/addon-mobile";
+import { TuiFloatingContainer } from "@taiga-ui/layout";
 
 @Component({
   selector: "app-new-transaction",
-  imports: [],
+  imports: [TuiButton, TuiSheetDialog, TuiFloatingContainer],
   templateUrl: "./new-transaction.html",
   styleUrl: "./new-transaction.scss",
 })
-export class NewTransaction {}
+export class NewTransaction implements OnInit {
+  protected open = false;
+
+  ngOnInit() {
+    this.open = true;
+  }
+
+  protected readonly options: Partial<TuiSheetDialogOptions> = {
+    label: "Alexander Inkin",
+    closable: false,
+  };
+}
