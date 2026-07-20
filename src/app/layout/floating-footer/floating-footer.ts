4b98302a25c39c1bf936417790973d679c090d33
Alessandro Esposito <alessandro.esposito251@gmail.com>
2026-07-20 17:36:01 +0200

    app setup

--- src/app/layout/floating-footer/floating-footer.ts
+++ src/app/layout/floating-footer/floating-footer.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
-import { TuiActionBar } from "@taiga-ui/kit";
-import { TuiButton, TuiIcon } from "@taiga-ui/core";
+import { TuiButtonGroup } from "@taiga-ui/kit";
+import { TuiAppearance, TuiIcon } from "@taiga-ui/core";
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-floating-footer",
-  imports: [TuiActionBar, TuiButton, RouterLink, TuiIcon],
+  imports: [RouterLink, TuiIcon, TuiAppearance, TuiButtonGroup],
   templateUrl: "./floating-footer.html",
   styleUrl: "./floating-footer.scss",
 })
