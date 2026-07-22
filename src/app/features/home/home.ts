9a340baf877db818a2dc805b74143680b0fb19e8
Alessandro Esposito <alessandro.esposito251@gmail.com>
2026-07-22 13:04:28 +0200

    homepage recap implementation

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,18 +1,40 @@
-import { AsyncPipe, JsonPipe } from "@angular/common";
-import { Component, inject } from "@angular/core";
+import { CurrencyPipe, DatePipe } from "@angular/common";
+import { Component, computed, inject, signal } from "@angular/core";
 import { Store } from "@ngrx/store";
-import { selectTransactionsState } from "../../state/transactions/transactions.selector";
+import { selectTotalsByMonth } from "../../state/transactions/transactions.selector";
+import { TuiHeader } from "@taiga-ui/layout";
+import { TuiButton, TuiTitle } from "@taiga-ui/core";
+import { AmountPipe } from "../../shared/pipes/amount-pipe";
+import { TuiStatus } from "@taiga-ui/kit";
 
 @Component({
   selector: "app-home",
-  imports: [AsyncPipe, JsonPipe],
+  imports: [
+    TuiHeader,
+    TuiButton,
+    TuiTitle,
+    AmountPipe,
+    CurrencyPipe,
+    DatePipe,
+    TuiStatus,
+  ],
   templateUrl: "./home.html",
   styleUrl: "./home.scss",
 })
 export class Home {
   private readonly store = inject(Store);
 
-  protected readonly transactionsState$ = this.store.select(
-    selectTransactionsState,
+  protected readonly currentMonth = signal(
+    new Date().toISOString().slice(0, 7),
+  );
+
+  protected monthIncomesExpenses = computed(() =>
+    this.store.selectSignal(selectTotalsByMonth(this.currentMonth()))(),
+  );
+
+  protected monthNet = computed(
+    () =>
+      this.monthIncomesExpenses().totalIncome -
+      this.monthIncomesExpenses().totalExpense,
   );
 }
--- src/app/state/transactions/transactions.selector.ts
+++ src/app/state/transactions/transactions.selector.ts
@@ -24,3 +24,35 @@ export const selectTransactionById = (id: string) =>
   createSelector(selectTransactions, (transactions) =>
     transactions.find((transaction) => transaction.id === id),
   );
+
+export const selectNetTotalByMonth = (month: string) =>
+  createSelector(selectTransactions, (transactions) =>
+    transactions
+      .filter((transaction) => transaction.occurredOn.startsWith(month))
+      .reduce(
+        (total, transaction) =>
+          total +
+          (transaction.type === "income"
+            ? transaction.amountInCents
+            : -transaction.amountInCents),
+        0,
+      ),
+  );
+
+export const selectTotalsByMonth = (month: string) =>
+  createSelector(selectTransactions, (transactions) => {
+    let totalIncome = 0;
+    let totalExpense = 0;
+    for (const transaction of transactions) {
+      if (!transaction.occurredOn.startsWith(month)) {
+        continue;
+      }
+
+      if (transaction.type === "income") {
+        totalIncome += transaction.amountInCents;
+      } else {
+        totalExpense += transaction.amountInCents;
+      }
+    }
+    return { totalIncome, totalExpense };
+  });
