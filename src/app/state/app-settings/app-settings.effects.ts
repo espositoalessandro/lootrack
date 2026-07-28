79f99214c26bee00516632461d7c01cebc08276a
Alessandro Esposito <alessandro.esposito251@gmail.com>
2026-07-28 13:34:22 +0200

    fix app settings repository

--- src/app/state/app-settings/app-settings.effects.ts
+++ src/app/state/app-settings/app-settings.effects.ts
@@ -27,23 +27,23 @@ export class AppSettingsEffects {
     this.actions$.pipe(
       ofType(loadAppSettings),
       switchMap(() =>
-        this.appSettingsDatabase
-          .get()
-          .pipe(map((settings) => loadAppSettingsSuccess({ settings }))),
-      ),
-      catchError((error) => {
-        if (error instanceof AppSettingsNotFoundError) {
-          return of(createSettingsDefaults());
-        }
-        return of(
-          loadAppSettingsFailure({
-            error:
-              error instanceof Error
-                ? error.message
-                : "Unable to load settings",
+        this.appSettingsDatabase.get().pipe(
+          map((settings) => loadAppSettingsSuccess({ settings })),
+          catchError((error) => {
+            if (error instanceof AppSettingsNotFoundError) {
+              return of(createSettingsDefaults());
+            }
+            return of(
+              loadAppSettingsFailure({
+                error:
+                  error instanceof Error
+                    ? error.message
+                    : "Unable to load settings",
+              }),
+            );
           }),
-        );
-      }),
+        ),
+      ),
     ),
   );
 
@@ -88,18 +88,18 @@ export class AppSettingsEffects {
     this.actions$.pipe(
       ofType(updateAppSettings),
       switchMap((input) =>
-        this.appSettingsDatabase
-          .update(input.newSettings)
-          .pipe(map((settings) => updateAppSettingsSuccess({ settings }))),
-      ),
-      catchError((error) =>
-        of(
-          updateAppSettingsFailure({
-            error:
-              error instanceof Error
-                ? error.message
-                : "Unable to load settings",
-          }),
+        this.appSettingsDatabase.update(input.newSettings).pipe(
+          map((settings) => updateAppSettingsSuccess({ settings })),
+          catchError((error) =>
+            of(
+              updateAppSettingsFailure({
+                error:
+                  error instanceof Error
+                    ? error.message
+                    : "Unable to load settings",
+              }),
+            ),
+          ),
         ),
       ),
     ),
