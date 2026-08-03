688da984e36c58340e93852f36186c5f0ba95cc5
Alessandro Esposito <alessandro.esposito251@gmail.com>
2026-08-03 17:30:13 +0200

    navbar moved

--- src/app/core/generated/build-info.ts
+++ src/app/core/generated/build-info.ts
@@ -1,5 +1,5 @@
 export const BUILD_INFO = {
-  version: "0.1.dev",
-  commitSha: "local",
-  builtAt: null,
+  "version": "0.1.dev",
+  "commitSha": "local",
+  "builtAt": "2026-08-03T13:13:21.799Z"
 } as const;
