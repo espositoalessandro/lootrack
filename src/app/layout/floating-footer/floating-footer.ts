fc8567d67d37ee912076f36c6a8a2607dc2a7233
Alessandro Esposito <alessandro.esposito251@gmail.com>
2026-08-13 15:41:36 +0200

    fix footer

--- src/app/layout/floating-footer/floating-footer.ts
+++ src/app/layout/floating-footer/floating-footer.ts
@@ -1,18 +1,18 @@
 import { Component } from "@angular/core";
-import { TuiIcon } from "@taiga-ui/core";
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 import { TuiTabBarComponent, TuiTabBarItem } from "@taiga-ui/addon-mobile";
+import { TuiButton } from "@taiga-ui/core";
 
 @Component({
   selector: "app-floating-footer",
-  imports: [RouterLink, TuiIcon, TuiTabBarComponent, TuiTabBarItem],
+  imports: [
+    RouterLink,
+    TuiTabBarComponent,
+    TuiTabBarItem,
+    TuiButton,
+    RouterLinkActive,
+  ],
   templateUrl: "./floating-footer.html",
   styleUrl: "./floating-footer.scss",
-  host: {
-    "[attr.data-platform]": '"ios"',
-    "[class.tui-liquid-glass]": "true",
-  },
 })
-export class FloatingFooter {
-  protected activeItemIndex = 1;
-}
+export class FloatingFooter {}
